ce0e1d399388645582aff3f1dbffbbc5be3e0f55
Kenzy Hamed <104325633+Kenzyhamed@users.noreply.github.com>
2023-03-10 15:04:15 -0700

    Update Editor.js

--- src/Editor.js
+++ src/Editor.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
-
+import 'react-quill/dist/quill.snow.css';
 
 function Editor() {
   const [value, setValue] = useState('');
